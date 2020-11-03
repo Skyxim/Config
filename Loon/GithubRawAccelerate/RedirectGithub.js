b960ba774b5c7305bdcd20ab4f8b798cbe819056
Skyxim <70276018+Skyxim@users.noreply.github.com>
2020-11-03 19:25:44 +0800

    Update RedirectGithub.js

--- Loon/GithubRawAccelerate/RedirectGithub.js
+++ Loon/GithubRawAccelerate/RedirectGithub.js
@@ -1,7 +1,7 @@
 let url=request.url
 const redirectHost=raw.staticdn.net
 function redirect(){
-    let path=url.replace(/https?:\/\/.*\.github\.com/,"")
+    let path=url.replace(/https?:\/\/github\.com/,"")
     let userAndRepository=path.substring(0,path.indexOf("raw"))
     let folderAndFile=path.substring(path.indexOf("raw")+4,path.length)
     return "https://"+redirectHost+userAndRepository+folderAndFile
